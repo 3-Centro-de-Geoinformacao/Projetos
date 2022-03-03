313bd029ceca49432f8dbbf726cb085fedc47c11
3-Centro-de-Geoinformacao <99277481+3-Centro-de-Geoinformacao@users.noreply.github.com>
2022-03-03 11:19:42 -0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -88,8 +88,125 @@ const INIT_ZOOM = {
 }
 
 var PROJECTS = {
+    'mapeamento-FORPRON': {
+        title: 'Vetorização de cartas para a certificação da FORPRON',
+        legend: [
+            1,
+            7,
+            8,
+            5,
+        ],
+        description: `O objetivo do presente projeto consiste em realizar a vetorização de 24 cartas para 1:50.000, 
+        no  intuito de apoiar a simulação construtiva da 10ª Bda Inf Mtz no contexto da certificação daquela GU como FORPRON
+        `,
+        lotes: [
+            {
+                name: 'mapeamento-FORPRON',
+                subtitle: 'vetorização da FORPRON 1:50.000',
+                zoom: [
+                    [-42, -10], // southwestern corner of the bounds
+                    [-33, -7] // northeastern corner of the bounds
+                ],
+                styles: [
+                    {
+                        'id': 'mapeamento-FORPRON-fill',
+                        'source': 'mapeamento-FORPRON',
+                        'type': 'fill',
+                        'layout': {},
+                        'paint': {
+                            'fill-opacity': 0.9
+                        }
+                    },
+                    {
+                        'id': 'mapeamento-FORPRON-border',
+                        'source': 'mapeamento-FORPRON',
+                        'type': 'line',
+                        'layout': {},
+                        'paint': {
+                            'line-color': '#050505',
+                            'line-width': 0.5
+                        }
+                    },
+                    {
+                        'id': 'mapeamento-FORPRON-text',
+                        'source': 'mapeamento-FORPRON',
+                        "type": "symbol",
+                        "maxzoom": 10,
+                        "minzoom": 7.4,
+                        'layout': {
+                            'text-field': ['to-string', ['get', 'identificador']]
+
+                        },
+                        'paint': {
+
+                        }
+                    }
+                ]
+            },
+            
+        ]
+    },
+    'mapeamento-topografico-cimnc': {
+        title: 'Mapeamento Topográfico do Campo de Instrução Marechal Newton Cavalcanti - CIMNC',
+        description: `O objetivo do presente projeto consiste em realizar o mapeamento topográfico do Campo de Instrução Marechal Newton Cavalcanti - CIMNC,
+        para gerar 4 cartas topográficas na escala 1:25.000 visando atender a meta do Plano Interno de Trabalho(PIT) 2022 da Diretoria de Serviço Geográfico.`,
+        legend: [
+            7,
+            4,
+            5,
+            6,
+            3,
+        ],
+       
+        lotes: [
+            {
+                name: 'mapeamento-topografico-cimnc',
+                subtitle: 'Mapeamento topográfico CIMNC',
+                zoom: [
+                    [-35.25, -8], // southwestern corner of the bounds
+                    [-34.9, -7.6] // northeastern corner of the bounds
+                ],
+                styles: [
+                    {
+                        'id': 'mapeamento-topografico-cimnc-fill',
+                        'source': 'mapeamento-topografico-cimnc',
+                        'type': 'fill',
+                        'layout': {},
+                        'paint': {
+                            'fill-opacity': 0.9
+                        }
+                    },
+                    {
+                        'id': 'mapeamento-topografico-cimnc',
+                        'source': 'mapeamento-topografico-cimnc',
+                        'type': 'line',
+                        'layout': {},
+                        'paint': {
+                            'line-color': '#050505',
+                            'line-width': 0.5
+                        }
+                    },
+                    {
+                        'id': 'mapeamento-topografico-cimnc',
+                        'source': 'mapeamento-topografico-cimnc',
+                        "type": "symbol",
+                        "maxzoom": 11.0,
+                        "minzoom": 9,
+                        'layout': {
+                            'text-field': ['to-string', ['get', 'identificador']]
+
+                        },
+                        'paint': {
+
+                        }
+                    }
+                ]
+            },
+            
+        ]
+    },
     'mapeamento-do-estado-de-pernambuco 1:25000': {
-        title: 'Mapeamento do Estado de Pernambuco 1:25.000',
+        title: 'Mapeamento do Estado de Pernambuco',
         legend: [
             1,
             12,
@@ -99,10 +216,10 @@ var PROJECTS = {
             6,
             3
         ],
-        description: `O objetivo do presente projeto consiste em realizar o mapeamento do Estado de Pernambuco,
-        para gerar 493 cartas ortoimagens na escala 1:25.000 visando atender a meta do Plano Interno de Trabalho (PIT) 2022 da Diretoria de Serviço Geográfico.
-        Vetorização: 55,78% 
-        Adequação: 18,05% 
+        description: `O objetivo do presente projeto consiste em realizar o mapeamento do estado de Pernambuco,
+        para gerar 493 cartas ortoimagens na escala 1:25.000 visando atender a meta do Plano Interno de Trabalho(PIT) 2022 da Diretoria de Serviço Geográfico.
+        Vetorização: 57,20% 
+        Adequação: 18,45% 
         Validação: 2,42% 
         Finalizadas: 1,42% `,
         lotes: [
@@ -154,8 +271,8 @@ var PROJECTS = {
     },
     'mapeamento-do-estado-de-pernambuco-1:50.000': {
         title: 'Mapeamento do Estado de Pernambuco 1:50.000',
-        description: `O objetivo do presente projeto consiste em realizar o mapeamento do Estado de Pernambuco,
-        para gerar  69 cartas ortoimagens na escala 1:50.000 visando atender a meta do Plano Interno de Trabalho (PIT) 2022 da Diretoria de Serviço Geográfico.`,
+        description: `O objetivo do presente projeto consiste em realizar o mapeamento do estado de Pernambuco,
+        para gerar  69 cartas ortoimagens na escala 1:50.000 visando atender a meta do Plano Interno de Trabalho(PIT) 2022 da Diretoria de Serviço Geográfico.`,
         legend: [
             1,
             12,
@@ -213,71 +330,11 @@ var PROJECTS = {
             
         ]
     },
-    'mapeamento-topografico-cimnc': {
-        title: 'Mapeamento Topográfico do Campo de Instrução Marechal Newton Cavalcanti - CIMNC',
-        description: `O objetivo do presente projeto consiste em realizar o mapeamento topográfico do Campo de Instrução Marechal Newton Cavalcanti - CIMNC,
-        para gerar 4 cartas topográficas na escala 1:25.000 visando atender a meta do Plano Interno de Trabalho (PIT) 2022 da Diretoria de Serviço Geográfico.`,
-        legend: [
-            1,
-            12,
-            7,
-            8,
-            5,
-            6,
-            3
-        ],
-       
-        lotes: [
-            {
-                name: 'mapeamento-topografico-cimnc',
-                subtitle: 'Mapeamento topográfico CIMNC',
-                zoom: [
-                    [-35.25, -8], // southwestern corner of the bounds
-                    [-34.9, -7.6] // northeastern corner of the bounds
-                ],
-                styles: [
-                    {
-                        'id': 'mapeamento-topografico-cimnc-fill',
-                        'source': 'mapeamento-topografico-cimnc',
-                        'type': 'fill',
-                        'layout': {},
-                        'paint': {
-                            'fill-opacity': 0.9
-                        }
-                    },
-                    {
-                        'id': 'mapeamento-topografico-cimnc',
-                        'source': 'mapeamento-topografico-cimnc',
-                        'type': 'line',
-                        'layout': {},
-                        'paint': {
-                            'line-color': '#050505',
-                            'line-width': 0.5
-                        }
-                    },
-                    {
-                        'id': 'mapeamento-topografico-cimnc',
-                        'source': 'mapeamento-topografico-cimnc',
-                        "type": "symbol",
-                        "maxzoom": 11.0,
-                        "minzoom": 9,
-                        'layout': {
-                            'text-field': ['to-string', ['get', 'identificador']]
-
-                        },
-                        'paint': {
-
-                        }
-                    }
-                ]
-            },
-            
-        ]
-    },
+    
     'mapeamento-das-capitais': {
         title: 'Mapeamento das Capitais',
         description: `O objetivo do presente projeto consiste em realizar o mapeamento das capitais, para gerar 41 cartas ortoimagens, 8 na escala 1:25.000 
-        e 33 na escala de 1:50.000, visando atender a meta do Plano Interno de Trabalho (PIT) 2022 da Diretoria de Serviço Geográfico. 
+        e 33 na escala de 1:50.000, visando atender a meta do Plano Interno de Trabalho(PIT) 2022 da Diretoria de Serviço Geográfico. /n
         Finalizadas: 19,51%`,
         legend: [
             1,
