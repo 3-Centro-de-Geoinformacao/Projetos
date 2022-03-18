00b0bef89c2ad16f470fe8b703366b739c36654c
3-Centro-de-Geoinformacao <99277481+3-Centro-de-Geoinformacao@users.noreply.github.com>
2022-03-18 09:44:30 -0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -12,8 +12,8 @@ const SUBTITLE_STATES = [
     },
     {
         id: 3,
-        name: 'Disseminação',
-        color: 'rgb(145,207,96)'
+        name: 'Finalizada',
+        color: 'rgb(51,204,51)'
     },
     {
         id: 4,
@@ -33,7 +33,7 @@ const SUBTITLE_STATES = [
     {
         id: 7,
         name: 'Vetorização',
-        color: 'rgb(222,119,174)'
+        color: 'rgb(255,255,0)'
     },
     {
         id: 8,
@@ -93,8 +93,7 @@ var PROJECTS = {
         legend: [
             1,
             7,
-            8,
-            5,
+            3,
         ],
         description: `O objetivo do presente projeto consiste em realizar a vetorização de 24 cartas para 1:50.000, 
         no  intuito de apoiar a simulação construtiva da 10ª Bda Inf Mtz no contexto da certificação daquela GU como FORPRON
